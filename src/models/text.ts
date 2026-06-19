0a3f186b16412254badc2df566853f330396e875
Petre Leonard Macamete <petreleonardos@gmail.com>
2026-06-20 01:16:25 +0300

    add export

--- src/models/text.ts
+++ src/models/text.ts
@@ -1,17 +1,17 @@
 import { type Collection, type DeleteResult, type InsertOneResult, type UpdateResult} from "mongodb";
 
-interface Text {
+export interface Text {
     _id?: string,
     title: string,
     content: string
 }
 
-async function addText (collection: Collection<Text>, text: Text): Promise<InsertOneResult<Text>> {
+export async function addText (collection: Collection<Text>, text: Text): Promise<InsertOneResult<Text>> {
     let response: InsertOneResult<Text> = await collection.insertOne(text);
     return response;
 }
 
-async function listAll(collection: Collection<Text>): Promise<Array<Text>> {
+export async function listAll(collection: Collection<Text>): Promise<Array<Text>> {
     let list: Array<Text> = [];
     let response = collection.find();
     for await (const text of response) {
@@ -20,12 +20,14 @@ async function listAll(collection: Collection<Text>): Promise<Array<Text>> {
     return list;
 }
 
-async function deleteOne(collection: Collection<Text>, id: string): Promise<DeleteResult> {
+export async function deleteOne(collection: Collection<Text>, id: string): Promise<DeleteResult> {
     let response = await collection.deleteOne({_id: id});
     return response;
 }
 
-async function replaceOne(collection: Collection<Text>, id: string, text: Text): Promise<UpdateResult<Text>> {
+export async function replaceOne(collection: Collection<Text>, id: string, text: Text): Promise<UpdateResult<Text>> {
     let response = await collection.replaceOne({_id: id}, text);
     return response;
 }
+
+
